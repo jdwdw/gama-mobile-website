877cc82eff2094392ca91d427bc67e47d5f2b1db
kejiayi <760220261@qq.com>
2017-06-15 17:58:24 +0800

    登录页面记住密码样式

--- project/src/js/login.js
+++ project/src/js/login.js
@@ -1,7 +1,44 @@
 
+ //正则匹配手机号
+  var number = /^1\d{10}$/
 
 
+function loadElement(){
+  var userName = document.getElementById('userName').value;
+  var passWord =  document.getElementById('passWord').value;
+  var surePassWord = document.getElementById('sure-passWord').value;
+  var verificationInput = document.getElementById('verificationInput').value;
+  var nextButton= document.getElementById('nextButton');
+  var verify=document.getElementById('verify');
+  var sureAmend = document.getElementById('sure-amend');
+  var sure=document.getElementById('sure');
+  return {
+    userName:userName,
+    passWord:passWord,
+    surePassWord:surePassWord,
+    verificationInput:verificationInput,
+    nextButton:nextButton,
+    verify:verify,
+    sureAmend:sureAmend,
+    sure:sure
+  }
 
+}
+function errorElement(connect){
+  var error = document.getElementById('error');
+  var errorConnect = document.getElementById('error-connect');
+  error.style.display = 'block';
+  errorConnect.innerHTML = connect;
+}
+
+
+// 弹窗定时消失
+function disappear(){
+  setTimeout(function(){
+    var error = document.getElementById('error');
+    error.style.display = 'none';
+  },1500)
+}
 
 //验证码倒计时
 function time() {
@@ -30,35 +67,44 @@ function time() {
 
 // 找回密码点击下一步
 function next(){
-  var userName = document.getElementById('userName').value;
-  var nextButton= document.getElementById('nextButton');
-  var verify=document.getElementById('verify');
-  var sureAmend = document.getElementById('sure-amend');
-  var sure=document.getElementById('sure');
+  var result=loadElement();
+  var stringData = {};
   var reset = document.getElementById('reset');
-  var error = document.getElementById('error');
-  var errorConnect = document.getElementById('error-connect');
   var resetIcon = reset.firstElementChild.lastElementChild.lastElementChild;
-  if(!userName){
-    nextButton.disabled = true;
-    nextButton.style.background='#999999';
+  if(!result.userName || !result.verificationInput){
+    result.nextButton.disabled = true;
+    result.nextButton.style.background='#999999';
+    setTimeout(function(){
+        document.getElementById('userName').addEventListener('input',function(event){
+          result.nextButton.disabled = false;
+          result.nextButton.style.background='#e22127';
+        })
+    },500)
+
   }else{
-    verify.style.display="none";
-    sure.style.display="block";
-    reset.style.color ='#e22127';
-    resetIcon.style.color ='#e22127';
-    sureAmend.addEventListener('click',function(){
-      var passWord =  document.getElementById('passWord').value;
-      var surePassWord = document.getElementById('sure-passWord').value;
-      if(surePassWord != passWord){
-        error.style.display = 'block';
-        errorConnect.innerHTML = '密码输入不一致!'
-      }
-    })
+    if(number.test(result.userName)){
+      result.verify.style.display="none";
+      result.sure.style.display="block";
+      reset.style.color ='#e22127';
+      resetIcon.style.color ='#e22127';
+      result.sureAmend.addEventListener('click',function(){
+         result=loadElement();
+        if(result.surePassWord != result.passWord){
+          errorElement('密码输入不一致!')
+        }else{
+          stringData.surePassWord=result.surePassWord ;
+          stringData.passWord=result.passWord ;
+          var jsonFormat = JSON.stringify(stringData);
+          localStorage.setItem(stringData.surePassWord, stringData.passWord);
+          console.log(stringData);
+        }
+      })
+    }else {
+      errorElement('输入手机号格式不对!');
+    }
   }
 }
 
-// 确认修改
 
 
 
@@ -72,21 +118,15 @@ function next(){
 // 注册页面
 function save() {
   var stringData = {};
-  var userName = document.getElementById('userName').value;
-  var passWord =  document.getElementById('passWord').value;
-  var surePassWord = document.getElementById('sure-passWord').value;
-  var verificationInput = document.getElementById('verificationInput').value;
-  var error = document.getElementById('error');
-  var errorConnect = document.getElementById('error-connect');
-  if(surePassWord != passWord ){
-    error.style.display = 'block';
-    errorConnect.innerHTML = '密码输入不一致!'
+  var result=loadElement();
+  if(result.passWord != result.surePassWord  ){
+      errorElement('密码输入不一致');
   }
-  stringData.userName = userName;
-  stringData.passWord = passWord;
-  stringData.surePassWord = surePassWord;
-  stringData.verificationInput = verificationInput;
-  var jsonFormat = JSON.stringify(stringData)
+  stringData.userName = result.userName;
+  stringData.passWord = result.passWord;
+  stringData.surePassWord = result.surePassWord;
+  stringData.verificationInput = result.verificationInput;
+  var jsonFormat = JSON.stringify(stringData);
   localStorage.setItem(stringData.userName, stringData.passWord);
   console.log(stringData)
 }
@@ -95,14 +135,11 @@ function save() {
 
 // 登录页面
 function findItem(){
-  var userName = document.getElementById('userName').value;
-  var passWord =  document.getElementById('passWord').value;
-  var error = document.getElementById('error');
-  var errorConnect = document.getElementById('error-connect');
-  var finduserName = localStorage.getItem(userName);
-  var findpassWord = localStorage.getItem(passWord);
-  if(!userName || !passWord || !finduserName || !findpassWord){
-    error.style.display = 'block';
-    errorConnect.innerHTML = '帐号或密码输入有误!'
+  var result = loadElement();
+  var finduserName = localStorage.getItem(result.userName);
+  var findpassWord = localStorage.getItem(result.passWord);
+  if(!result.userName || !result.passWord || !finduserName || !findpassWord){
+    errorElement('帐号或密码输入有误!');
+    // disappear();
   }
 }
