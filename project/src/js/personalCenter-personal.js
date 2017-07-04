aba76ad0ac92c359594e48e97bd06a532502f523
hopperhuang <huanjiwe@126.com>
2017-07-04 10:02:01 +0800

    更改了personalcneter中的相關文件。將原來的js跳轉改為a標簽跳轉，刪除了相關js文件中js跳轉的函數方法，更改了相關css的樣式。

--- project/src/js/personalCenter-personal.js
+++ project/src/js/personalCenter-personal.js
@@ -4,25 +4,25 @@ signature.onclick = function () {
   this.innerHTML = "已签到";
   this.style.backgroundColor = "#CCCCCC";
 }
-var modifyPersonalMessage = document.querySelector(".head-and-tool-head");
-// modifyPersonalMessage.onclick = function () {
-//   var url = this.dataset.url;
-//   console.log(url);
-//   window.location.href=url;
+// var modifyPersonalMessage = document.querySelector(".head-and-tool-head");
+// // modifyPersonalMessage.onclick = function () {
+// //   var url = this.dataset.url;
+// //   console.log(url);
+// //   window.location.href=url;
+// // }
+// function jump(htmlTarget) {
+//   htmlTarget.onclick = function () {
+//     var url = this.dataset.url;
+//     console.log(url);
+//     window.location.href=url;
+//   }
 // }
-function jump(htmlTarget) {
-  htmlTarget.onclick = function () {
-    var url = this.dataset.url;
-    console.log(url);
-    window.location.href=url;
-  }
-}
-jump(modifyPersonalMessage);
-var toolItem = document.querySelectorAll(".tool-item");
-for (let i = 0; i < toolItem.length; i++) {
-  jump(toolItem[i]);
-}
-var message_password_consult_item = document.querySelectorAll('.message-password-consult-item');
-for (let i = 0; i < message_password_consult_item.length; i++) {
-  jump(message_password_consult_item[i]);
-}
+// jump(modifyPersonalMessage);
+// var toolItem = document.querySelectorAll(".tool-item");
+// for (let i = 0; i < toolItem.length; i++) {
+//   jump(toolItem[i]);
+// }
+// var message_password_consult_item = document.querySelectorAll('.message-password-consult-item');
+// for (let i = 0; i < message_password_consult_item.length; i++) {
+//   jump(message_password_consult_item[i]);
+// }
